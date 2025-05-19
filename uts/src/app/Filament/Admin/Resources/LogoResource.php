45c99197a87f09c1fad83eb169ba06bf0d2f4185
Raffi <colabyes4@gmail.com>
2025-05-19 23:40:25 +0700

    Add UTS project files

---
+++ uts/src/app/Filament/Admin/Resources/LogoResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Admin\Resources;
+
+use App\Filament\Admin\Resources\LogoResource\Pages;
+use App\Filament\Admin\Resources\LogoResource\RelationManagers;
+use App\Models\Logo;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class LogoResource extends Resource
+{
+    protected static ?string $model = Logo::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListLogos::route('/'),
+            'create' => Pages\CreateLogo::route('/create'),
+            'edit' => Pages\EditLogo::route('/{record}/edit'),
+        ];
+    }
+}
